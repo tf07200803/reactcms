b7631476afb6e4b3a35ac275e209125fc97dda44
tommy <tt@gg.cc>
2020-12-30 15:05:19 +0800

    膽案更新
    膽案更新

--- templates/default/content/reactapp/build/static/js/runtime-main.9b1507f9.js
+++ templates/default/content/reactapp/build/static/js/runtime-main.9b1507f9.js
@@ -1,42 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <link rel="icon" href="./phpcms/templates/default/content/reactapp/build/favicon.ico">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    <meta name="theme-color" content="#000000">
-    <meta name="description" content="Web site created using create-react-app">
-    <link rel="apple-touch-icon" href="./phpcms/templates/default/content/reactapp/build/logo192.png">
-    <!--
-      manifest.json provides metadata used when your web app is installed on a
-      user's mobile device or desktop. See https://developers.google.com/web/fundamentals/web-app-manifest/
-    -->
-    <link rel="manifest" href="./phpcms/templates/default/content/reactapp/build/manifest.json">
-    <!--
-      Notice the use of ./phpcms/templates/default/content/reactapp/build in the tags above.
-      It will be replaced with the URL of the `public` folder during the build.
-      Only files inside the `public` folder can be referenced from the HTML.
-
-      Unlike "/favicon.ico" or "favicon.ico", "./phpcms/templates/default/content/reactapp/build/favicon.ico" will
-      work correctly both with client-side routing and a non-root public URL.
-      Learn how to configure a non-root public URL by running `npm run build`.
-    -->
-    <title>React App</title>
-  <link href="./phpcms/templates/default/content/reactapp/build/static/css/2.43b2b750.chunk.css" rel="stylesheet"><link href="./phpcms/templates/default/content/reactapp/build/static/css/main.a277eeae.chunk.css" rel="stylesheet"></head>
-  <body>
-    <noscript>You need to enable JavaScript to run this app.</noscript>
-    <div id="root"></div>
-    <!--
-      This HTML file is a template.
-      If you open it directly in the browser, you will see an empty page.
-
-      You can add webfonts, meta tags, or analytics to this file.
-      The build step will place the bundled scripts into the <body> tag.
-
-      To begin the development, run `npm start` or `yarn start`.
-      To create a production bundle, use `npm run build` or `yarn build`.
-    -->
-  <script>/******/ (function(modules) { // webpackBootstrap
+/******/ (function(modules) { // webpackBootstrap
 /******/ 	// install a JSONP callback for chunk loading
 /******/ 	function webpackJsonpCallback(data) {
 /******/ 		var chunkIds = data[0];
@@ -259,5 +221,4 @@
 /******/ })
 /************************************************************************/
 /******/ ([]);
-//# sourceMappingURL=runtime-main.9b1507f9.js.map</script><script src="./phpcms/templates/default/content/reactapp/build/static/js/2.6a8ce30f.chunk.js"></script><script src="./phpcms/templates/default/content/reactapp/build/static/js/main.0fd28cd1.chunk.js"></script></body>
-</html>
+//# sourceMappingURL=runtime-main.9b1507f9.js.map
